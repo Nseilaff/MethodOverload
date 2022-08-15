eef2c17427055b18db72dca058607944898d2af8
Nate S <seilaffn@gmail.com>
2022-08-15 13:54:23 -0600

    First commit

---
+++ MethodOverload/Method.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    internal class Method
+    {
+        public static int Add(int num1, int num2)
+        {
+            
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            var sum = num1 + num2;
+            if (isTrue && sum > 1)
+            {
+                return $"{num1} + {num2} = {sum} dollars.";
+
+            }
+            else if (isTrue && sum == 1)
+            {
+                return $"{num1} + {num2} = {sum} dollar.";
+
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+    }
+}
---
+++ MethodOverload/Program.cs
@@ -0,0 +1,4 @@
+﻿using MethodOverload;
+Console.WriteLine(Method.Add(2,2));
+Console.WriteLine(Method.Add(2.134m, 2.144m));
+Console.WriteLine(Method.Add(32, 14, true));
